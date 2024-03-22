befbcfe3fc0839ab37adb2d347fc961117aaa71b
ethanhoopes <hoopesew@gmail.com>
2024-03-22 17:38:01 -0600

    finished up

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -26,6 +26,13 @@ namespace BookLibraryProject.Infrastructure
 
         public Paginationinfo PageModel { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+
+        public string PageClassNormal { get; set; } = String.Empty;
+
+        public string PageClassSelected { get; set; } = String.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -38,6 +45,12 @@ namespace BookLibraryProject.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
 
                     result.InnerHtml.AppendHtml(tag);
